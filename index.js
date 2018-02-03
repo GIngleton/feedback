79c540190fcc340ecd07a6aeee793c700914a798
gingleton <georgeingleton@gmail.com>
2018-02-03 11:31:54 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ you: 'can do it!' });
+  res.send({ big: '!' });
 });
 
 // Dynamic Port Binding
